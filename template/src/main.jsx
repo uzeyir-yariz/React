d408562ecd6ae98ba9a5f0d54598974648e8d427
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-08-25 16:25:03 +0300

    change

--- template/src/main.jsx
+++
@@ -1,9 +0,0 @@
-// import { StrictMode } from 'react' // burada ki kod iki defa console çağırıyor bunu silerek engelleyilebilirsin
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  // <StrictMode> bunlarıda silmek gerekyiyor
-    <App />,
-  // </StrictMode>
-)
